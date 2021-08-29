30a6fe48613713a69665a8867a74cdbd69db2348
Gai <german.isaev@gmail.com>
2021-08-29 08:15:31 +0300

    Update chart.service.ts
    remove comments

--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -5,34 +5,22 @@ import { ChartFailure, ChartSuccess } from '@app/store/actions';
 import { environment } from '@environments/environment';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-//import { EntityCollectionServiceBase, EntityCollectionServiceElementsFactory } from '@ngrx/data';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChartService {//extends EntityCollectionServiceBase<Chart> {
+export class ChartService {
 
   private BASE_URL = environment.chartApi;
-
-  // private http: HttpClient
-  /* constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory) {
-    super('Chart', serviceElementsFactory);
-   } */
-
-   constructor(private http: HttpClient) {}
+  
+  constructor(private http: HttpClient) {}
 
   getChart(): Observable<Chart> {
     const url = `${this.BASE_URL}`;
     return this.http.get<Chart>(url).pipe(
       map((data: any) => { 
         return data.Value; 
-        //return new ChartSuccess({ payload: data });
-      })//,
-      /* catchError((error) => {
-        return of(
-          new ChartFailure({ error: error })
-        )
-      }) */
+      })
     );
   }
 
